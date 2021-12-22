e668d8738904843892defbea40dca32fcfa339af
AliTavakoliNiya <81316112+AliTavakoliNiya@users.noreply.github.com>
2021-12-22 16:36:37 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import torch
 from args_fusion import args
-from scipy import misc
+from scipy.misc import imread, imsave, imresize
 import matplotlib as mpl
 
 from os import listdir
